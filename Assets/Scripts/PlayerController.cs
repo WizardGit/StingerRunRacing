ef3670b4c1ce3f17640ce7f73010d49a13c0e304
WizardGit <slocumkaiser@gmail.com>
2022-04-12 21:09:17 -0700

    Tweaking Some stats

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerRotationSpeed = 20f;
+    public Vector3 playerRotationSpeed = new Vector3(0, 100, 0);
     public float playerSpeed = 10f;
 
     private float movementX;
     private float movementY;
+    Vector3 m_Movement;
+    Vector2 inputDirection;
 
     Animation m_Animator;
+    Rigidbody rb;
 
     void Start()
     {
         m_Animator = GetComponent<Animation>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        inputDirection = movementVector.normalized;
 
         movementX = movementVector.x;
-        movementY = movementVector.y;
-
+        movementY = movementVector.y;        
     }
+
     private void OnJump()
     {
-        Vector3 jump = new Vector3(movementX, 400.0f, movementY);
-        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 jump = new Vector3(movementX, 400.0f, movementY);        
         rb.AddForce(jump);
         m_Animator.Play("Jump01");
     }
@@ -45,7 +49,11 @@ public class PlayerController : MonoBehaviour
         else
             m_Animator.Play("IdleHappy");
 
-        transform.Translate(0, 0, movementY * playerSpeed * Time.deltaTime);
-        transform.Rotate(0, movementX * playerRotationSpeed * Time.deltaTime, 0);
+        //transform.Translate(0, 0, movementY * playerSpeed * Time.deltaTime);
+        //transform.Rotate(0, movementX * playerRotationSpeed * Time.deltaTime, 0);
+
+        Quaternion deltaRotation = Quaternion.Euler(movementX * playerRotationSpeed * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+        rb.MovePosition(rb.position + transform.forward * playerSpeed * movementY * Time.deltaTime);
     }
 }
