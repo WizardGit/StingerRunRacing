e26d124421d5a9b68d62ae5ad8446149f1ac130b
WizardGit <slocumkaiser@gmail.com>
2022-04-14 08:22:30 -0700

    Updated camera! Now 'tab' key allows users to look behind their dragon object

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,6 +1,11 @@
+/* Created by Kaiser Slocum
+ * Last Edited on 4/14/2022 by Kaiser Slocum
+ * Purpose: To constantly modify the camera's positioning on the object
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -10,16 +15,46 @@ public class CameraFollow : MonoBehaviour
 
     public bool lookAt = true;
 
-    private void Update()
-    {
-        transform.position = target.TransformPoint(offsetPosition);
+    private bool check = false;
 
-        // compute rotation
+    public void OnFrontView()
+    {
         if (lookAt)
         {
+            // If we're looking at the object, we need to get in front of it (so we'll reverse our z offset
+            offsetPosition.z = offsetPosition.z * -1.0f;
+        }  
+        else
+        {
+            // We need to signal to Update method to rotate by 180 degrees
+            check = !check;
+        }
+    }
+
+    private void Update()
+    {        
+        // Get where our camera needs to be
+        transform.position = target.TransformPoint(offsetPosition);
+
+        // If we want to actually look at the object...
+        if (lookAt)
+        {
+            // Then Calculate camera rotation
             transform.LookAt(target);
         }
         else
-            transform.rotation = target.rotation;
+        {
+            if (check)
+            {
+                // The camera needs to be in our object but rotated by 180 degrees
+                Vector3 targetRotation = target.rotation.eulerAngles;
+                Vector3 transformRotation = new Vector3(targetRotation.x, targetRotation.y + 180, targetRotation.z);
+                transform.rotation = Quaternion.Euler(transformRotation);
+            }
+            else
+            {
+                transform.rotation = target.rotation;
+            }            
+        }            
     }
 }
